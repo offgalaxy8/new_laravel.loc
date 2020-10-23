34cd8e5ee68022a1350bcd51617ec667edd17838
offgalaxy8 <offgalaxy8@gmail.com>
2020-10-23 11:06:37 +0300

    add text at home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="home-text">
         <h1>Главная страница</h1>
-        <h3>Проект работает через github!!!</h3>
+        <h3>Проект работает через github!!! 23/10/2020</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto delectus doloremque exercitationem facilis, fugiat possimus voluptates voluptatibus! Architecto asperiores culpa dolorum laborum libero molestias, obcaecati omnis perspiciatis sit. Quae, sequi!</p>
     </div>
 @endsection
