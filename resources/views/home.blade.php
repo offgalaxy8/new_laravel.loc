6b1d6c029a74662a7f50cd3b805af52a52b748cd
Srgei <offgalaxy8@gmail.com>
2020-10-23 10:46:17 +0300

    Laravel copy - uniziped

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title_doc')Главная страница@endsection
+
+@section('content')
+    <div class="home-text">
+        <h1>Главная страница</h1>
+        <h3>Проект работает через github!!</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto delectus doloremque exercitationem facilis, fugiat possimus voluptates voluptatibus! Architecto asperiores culpa dolorum laborum libero molestias, obcaecati omnis perspiciatis sit. Quae, sequi!</p>
+    </div>
+@endsection
+
+@section('aside')
+    @parent
+    <p>Дополнительный текст на главной</p>
+@endsection
+
+
