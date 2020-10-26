a6883522dc813cde47800bb39a4a697273d18e2e
offgalaxy8 <56636426+offgalaxy8@users.noreply.github.com>
2020-10-26 23:10:37 +0300

    lesson 7 16.48

---
+++ resources/views/one-message.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title_doc'){{ $data->subject }} @endsection
+
+@section('content')
+    <div class="home-text">
+        <h1>{{ $data->subject }}</h1>
+
+            <div class="alert alert-info">
+                <h3>{{ $data->subject }}</h3>
+                <p>{{ $data->message }}</p>
+                <p>{{ $data->email }}</p>
+                <p>{{ $data->name }}</p>
+                <p><small>{{ $data->created_at }}</small></p>
+                <a href="{{ route('contact-update', $data->id) }}"><button class="btn btn-primary">Редактировать</button></a>
+            </div>
+
+    </div>
+@endsection
+
+@section('aside')
+    @parent
+    <p>Дополнительный текст на главной</p>
+@endsection
